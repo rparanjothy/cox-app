8c43f4748ddd13eacfa7912468c5101ffd2656e4
rxp8764 <ramkumar_paranjothy@homedepot.com>
2019-03-26 17:55:38 -0500

    Updates

--- precache-manifest.8eeedce86dc3185a1fadd3ef50ebd3f0.js
+++ precache-manifest.8eeedce86dc3185a1fadd3ef50ebd3f0.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/cox-app/static/js/runtime~main.dbad7e00.js"
   },
   {
-    "revision": "ffe6829e7f70260d47c3",
-    "url": "/cox-app/static/js/main.d136ddae.chunk.js"
+    "revision": "182d019aa796e15959d0",
+    "url": "/cox-app/static/js/main.2c2ee8a8.chunk.js"
   },
   {
     "revision": "c524de3c4c089b8baffe",
     "url": "/cox-app/static/js/2.e42eadff.chunk.js"
   },
   {
-    "revision": "ffe6829e7f70260d47c3",
+    "revision": "182d019aa796e15959d0",
     "url": "/cox-app/static/css/main.002ffd43.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/cox-app/static/css/2.9900ef0d.chunk.css"
   },
   {
-    "revision": "7a4830b79f6531bb7796848aaa5386e0",
+    "revision": "5f76d36976e7e467109a54516b475bab",
     "url": "/cox-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cox-app/precache-manifest.975530cdcf52a55c2d0f27085a8e917c.js"
+  "/cox-app/precache-manifest.8eeedce86dc3185a1fadd3ef50ebd3f0.js"
 );
 
 workbox.clientsClaim();
