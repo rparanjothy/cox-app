1f85372b4eeeab101868d7f1d5296f2b69a062c4
rxp8764 <ramkumar_paranjothy@homedepot.com>
2019-03-26 13:21:31 -0500

    added CSS and alerts

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
-import { Table } from "reactstrap";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Table, Alert } from "reactstrap";
 
 class App extends Component {
   constructor(props) {
@@ -9,35 +9,59 @@ class App extends Component {
     this.arrange = this.arrange.bind(this);
     this.removeElement = this.removeElement.bind(this);
     this.state = { show: 0, hasData: 0, done: 0 };
+    this.state = {
+      visible: true
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss() {
+    this.setState({ visible: false });
   }
 
   arrange(x) {
+    /*
+    Problem:
+    Create an application that accepts words from a user and sorts the words alphabetically into four columns, vertically, then horizontally. 
+    The last row should be the only row that contains any empty cells if the number of words are not evenly divisible by 4. 
+    */
+
+    /*
+    Solution:
+    1. based on the length of the words array entered by the user, compute the max number of rows for building a nX4 matrix
+    2. Once the rows count is determined, compute the total elements in the matrix, 
+    3. Identify the partially filled column counts by subtracting the length of words array from the length of the max elements possisble in the Nx4 matrix
+    4. Once the number of partially filled columns are determined, compute the fully filled columns by subtracting the partial column count from 4 (4 is fixed columns count for this problem)
+    5. Now, slice the words array to fill the fully filled columns, push to a new array
+    6. Then, slice the left overs to fill the partially filled columns, Since the last row is the only one to have missing values/ blanks, reduce the row count by -1 , push to a new array
+    7. Transpose the new array and generate a JSON
+    8. Render the JSON as a table of buttons
+    9. Add a remove element event to the buttons, and as they are removed, refresh the master and arrange again.
+    10. When we have removed all the element, set appropriate flags to render alerts
+    */
     var data = [...x];
     var words = [];
     var d = [];
-    console.log("Inside Arrange");
-    console.log(`master - ${this.state.master}`);
+    // console.log("Inside Arrange");
+    // console.log(`master - ${this.state.master}`);
 
     console.log("Sorting");
     if (data.length > 0) {
-      var words = data;
+      words = data;
       words = words.sort();
       this.setState({ words }, () => {
-        console.log("words set");
+        // console.log("words set");
       });
-      console.log(words);
 
-      console.log(words.length);
       // see if it is a multiple of 4
       // get the rows count
       const row = Math.ceil(words.length / 4);
       // log(`Max Row count is ${row}`);
-
-      // log(`Matrix is ${row * 4}`);
+      // log(`Matrix size is ${row * 4}`);
       var emptyColCt = row * 4 - words.length;
       var filledColCt = 4 - emptyColCt;
-      // log(`Empty column = ${(row*4)-data.length}`)
-      // log(`Filled column = ${4-((row*4)-data.length)}`)
+
       // log(`emptyColCt = ${emptyColCt}`);
       // log(`filled Col Ct = ${filledColCt}`);
 
@@ -64,7 +88,9 @@ class App extends Component {
       });
     } else {
       // when empty set done to on
-      this.setState({ show: 0, done: 1 });
+      this.setState({ show: 0, done: 1, hasData: 0, txt: "", visible: true });
+
+      //reset the text
     }
   }
 
@@ -82,6 +108,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <header className="App-header">
+          Sort and Arrange words in an N X 4 matrix - Ramkumar Paranjothy
+        </header>
         <div className="container">
           <Form
             onSubmit={e => {
@@ -90,42 +119,75 @@ class App extends Component {
             }}
           >
             <FormGroup>
-              <Label for="elements">
-                Enter your words to be sorted and arranged in a nX4 matrix
+              <Label className="info">
+                <b>Instructions:</b>
+                <ul>
+                  <li>
+                    Enter your words below to be sorted and arranged in a nX4 matrix
+                  </li>
+                  <li> Click "Arrange" / Enter to arrange </li>
+                  <li>Click a word to remove it</li>
+                  <li>Click "Reset" to start over</li>
+                </ul>
               </Label>
-              <br />
-              <Label for="elements">Click a word to remove </Label>
-
               <Input
                 type="text"
                 name="elements"
-                placeholder="apple orange grapes"
-                onChange={e =>
-                  this.setState({
-                    data: e.target.value.split(" "),
-                    master: e.target.value.split(" "),
-                    hasData: 1
-                  })
-                }
+                value={this.state.txt}
+                // while entering data, set done to false, hasData to true
+                onChange={e => {
+                  //validate for text
+                  var { value } = e.target;
+                  //split only when we have valid text
+                  if (value) {
+                    this.setState({
+                      data: value.split(" "),
+                      master: value.split(" "),
+                      hasData: 1,
+                      done: 0,
+                      txt: value
+                    });
+                  }
+                }}
               />
 
               {this.state.hasData ? (
                 <div>
                   <Button
-                    sz="sm"
-                    block
+                    style={{ marginTop: "10px" }}
+                    sz="md"
+                    // block
                     onClick={e => this.arrange(this.state.data)}
                   >
                     Arrange
+                  </Button>
+                  <Button
+                    style={{ marginTop: "10px", marginLeft: "10px" }}
+                    sz="md"
+                    // block
+                    onClick={e =>
+                      this.setState({ hasData: 0, txt: "", show: 0 })
+                    }
+                  >
+                    Reset
                   </Button>
                 </div>
               ) : null}
             </FormGroup>
           </Form>
 
-          {this.state.done ? "All elements removed !!" : null}
+          {/* render only when we are out of elements in the array */}
+          {this.state.done ? (
+            <Alert
+              color="danger"
+              isOpen={this.state.visible}
+              toggle={this.onDismiss}
+            >
+              All elements removed !!{" "}
+            </Alert>
+          ) : null}
 
-          <Table>
+          <Table borderless className="container">
             <tbody>
               {this.state.show
                 ? this.state.result.map((e, idx) => (
